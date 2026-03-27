5d089f8cc76c6ea56763fa068eb071682314fb84
Rakha Avilla <azmyervawanrakhaavilla@gmail.com>
2026-03-27 16:55:09 +0700

    first commit

--- src/main/java/com/mycompany/loopdowhilee/LoopDoWhilee.java
+++ src/main/java/com/mycompany/loopdowhilee/LoopDoWhilee.java
@@ -4,13 +4,33 @@
 
 package com.mycompany.loopdowhilee;
 
-/**
- *
- * @author ASUS
- */
-public class LoopDoWhilee {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+public class LoopDoWhilee {
+     public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+         int pilihan = 0;
+         
+         do  {
+             System.out.println("/n===MENU APLIKASI ===");
+             System.out.println("1. Tambah Data");
+             System.out.println("2. Lihat Data");
+             System.out.println("3. Keluar");
+             pilihan = scan.nextInt();
+             
+             switch(pilihan) {
+                 case 1: System.out.println("Data Ditambahkan...");break;
+                 case 2: System.out.println("Menampilkan Data...");break;
+                 case 3: System.out.println("Terima kasih...");break;
+                 default: System.out.println("Pilihan tidak ada");break;       
+             }
+         } while (pilihan != 3); // Ulangi selama pilihan bukan 3 (keluar)
+         
+         scan.close();
+             
+             
+         }    
+        
     }
-}
+
